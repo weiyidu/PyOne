c12584052ac25f42e6f727110bf388c9c2bd344b
Abbey <tumbzzc@qq.com>
2019-03-20 15:17:02 +0800

    fix cache for redis

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #-*- coding=utf-8 -*-
-from self_config import config_dir,password
+from self_config import *
 import os
 from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,8 +8,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class config:
     SECRET_KEY = os.path.join(config_dir,'PyOne'+password)
     CACHE_TYPE='redis'
+    CACHE_REDIS_HOST=REDIS_HOST
+    CACHE_REDIS_PORT=REDIS_PORT
+    CACHE_REDIS_DB=REDIS_DB
+    if REDIS_PASSWORD!='':
+        CACHE_REDIS_PASSWORD=REDIS_PASSWORD
     SEND_FILE_MAX_AGE_DEFAULT=timedelta(seconds=1)
-    version='4.190319'
+    version='4.190320'
 
     @staticmethod
     def init_app(app):
